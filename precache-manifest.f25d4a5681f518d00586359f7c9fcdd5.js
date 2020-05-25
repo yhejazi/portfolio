5310fa1c64e9b28ed4487ea095616997f14deaae
yhejazi <yhejazi@uw.edu>
2020-05-25 14:10:01 -0700

    Updates

---
+++ precache-manifest.f25d4a5681f518d00586359f7c9fcdd5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a207bf554bb50a3118348a9f1ed743b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6329dd2939371ff53222",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "02f401284323aa3bd16f",
+    "url": "/static/css/main.b567ea4c.chunk.css"
+  },
+  {
+    "revision": "6329dd2939371ff53222",
+    "url": "/static/js/2.acb0faf6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.acb0faf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f401284323aa3bd16f",
+    "url": "/static/js/main.c0208b7e.chunk.js"
+  },
+  {
+    "revision": "9b4452dcf5ff001e0742",
+    "url": "/static/js/runtime-main.79c938e6.js"
+  },
+  {
+    "revision": "d304f62c4f751c0743b2165ea50c96d6",
+    "url": "/static/media/banner-bg.d304f62c.jpg"
+  },
+  {
+    "revision": "2018bc20ab20338a58aeb32c8d8eda8a",
+    "url": "/static/media/funko-logo.2018bc20.svg"
+  },
+  {
+    "revision": "1c685462440706d23fb47c95cedc2fb4",
+    "url": "/static/media/nike-logo.1c685462.svg"
+  },
+  {
+    "revision": "5b7fc62c757297f1b4d918048c90d5ef",
+    "url": "/static/media/pemco-logo.5b7fc62c.svg"
+  },
+  {
+    "revision": "cb2cb974b17ac04300bf30e5a2061113",
+    "url": "/static/media/profile.cb2cb974.jpg"
+  },
+  {
+    "revision": "67c527dce3f39e84ec65d9dab32ef4ee",
+    "url": "/static/media/seiu-logo.67c527dc.svg"
+  },
+  {
+    "revision": "64a2be4d6e3624a6b8d3822b8737cda8",
+    "url": "/static/media/udacity-logo.64a2be4d.svg"
+  },
+  {
+    "revision": "1979e9249dc33ceeb54b288145468e43",
+    "url": "/static/media/uw-logo.1979e924.svg"
+  },
+  {
+    "revision": "4030f5fc01494fafdac06cfe57afe3fa",
+    "url": "/static/media/yasmine-hejazi-resume.4030f5fc.pdf"
+  }
+]);
